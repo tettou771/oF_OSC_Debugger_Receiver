8f32dac85d620b6371bcc67e15f42f8523bb9e22
Toru Takata <tettou771@gmail.com>
2019-08-12 02:12:49 +0900

    osc receive and display

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,17 +2,48 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+	ofSetBackgroundAuto(true);
+	ofSetBackgroundColor(40);
 
+	port.addListener(this, &ofApp::portChanged);
+
+	gui.setup();
+	gui.add(port.setup("OSC Port", 6000));
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+	// osc receive
+	while (osc.hasWaitingMessages()) {
+		ofxOscMessage message;
+		osc.getNextMessage(message);
 
+		string addr = message.getAddress();
+		string typestr = message.getTypeString();
+
+		stringstream debugMessage;
+		debugMessage << "OSC received: " << addr;
+		debugMessage << " " << typestr;
+		for (int i = 0; i < typestr.length(); ++i) {
+			char c = typestr[i];
+			switch (c) {
+			case 's': debugMessage << ' ' << message.getArgAsString(i); break;
+			case 'i': debugMessage << ' ' << message.getArgAsInt(i); break;
+			case 'f': debugMessage << ' ' << message.getArgAsFloat(i); break;
+			case 'd': debugMessage << ' ' << message.getArgAsDouble(i); break;
+			case 'T': debugMessage << ' ' << "True"; break;
+			case 'F': debugMessage << ' ' << "False"; break;
+			default: debugMessage << ' ' << "Unknown"; break;
+			}
+		}
+
+		ofxTextFlow::addText(debugMessage.str());
+	}
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+	gui.draw();
 }
 
 //--------------------------------------------------------------
@@ -63,6 +94,10 @@ void ofApp::windowResized(int w, int h){
 //--------------------------------------------------------------
 void ofApp::gotMessage(ofMessage msg){
 
+}
+
+void ofApp::portChanged(int & _port) {
+	osc.setup(_port);
 }
 
 //--------------------------------------------------------------
